600d2c41d064f38f78243ba098e5a0aa9637ee03
dqhan <291927006@qq.com>
2019-07-07 01:34:19 +0800

    peoplepicker

--- widget.js
+++ widget.js
@@ -901,8 +901,11 @@
     var PeoplePicker = function (ops) {
         this._ops = {
             items: ops.items,
-            selectedItem: ops.selectedItem
+            selectedItem: ops.selectedItem,
+            hashItems: {},
+            filterSelectionResultIndex: []
         };
+        this._ops.hashItems = this._convetToHash();
         this._element = ops.element;
         this._initId()
             ._init()
@@ -911,20 +914,32 @@
             ._bindEvent();
     }
     PeoplePicker.prototype = {
+        _convetToHash: function () {
+            var hash = {},
+                items = this._ops.items,
+                len = this._ops.items.length;
+            for (var i = 0; i < items.length; i++) {
+                if (!hash[items[i].id]) {
+                    hash[items[i].id] = items[i].name;
+                }
+            }
+            return hash;
+        },
         _initId: function () {
             uuid++;
             this._peoplePickerId = "ui-people-picker-" + uuid;
+            this._peoplePickerPopupId = "ui-people-picker-popup-" + uuid;
             return this;
         },
         _init: function () {
             $(this._element).addClass('ui-people-picker');
+            this._element.id = this._peoplePickerId;
             return this;
         },
         _create: function () {
             var _input = this._createInput();
             $(this._element).append(_input);
             this._createPopup();
-            this._setPopupPosition();
             return this;
         },
         _createInput: function () {
@@ -940,34 +955,46 @@
         },
         _createPopup: function () {
             var fragement = [], h = -1;
-            fragement[++h] = "<div class=\"ui-people-picker-dropdown\">";
+            fragement[++h] = "<div id=" + this._peoplePickerPopupId + " class=\"ui-people-picker-dropdown\" style=\"display:none\">";
             for (var i = 0; i < this._ops.items.length; i++) {
-                fragement[++h] = "<div class=\"ui-people-picker-dropdown-item\">" + this._ops.items[i].name + "</div>";
+                fragement[++h] = "<div class=\"ui-people-picker-dropdown-selection-container\">"
+                fragement[++h] = "<div class=\"ui-people-picker-dropdown-selection-item\">" + this._ops.items[i].name + "</div>";
+                fragement[++h] = "</div>";
             }
-            $dropdown_el.append(fragement.join(''));
             fragement[++h] = "</div>";
-            $(body).append(fragement.join(''));
+            $('body').append(fragement.join(''));
             return this;
         },
 
         _initMember: function () {
             this.$input = $('#' + this._peoplePickerId + " .ui-people-picker-container-main");
-            this.$popup = $('#' + this._peoplePickerId + " .ui-people-picker-dropdown");
+            this.$popup = $('#' + this._peoplePickerPopupId);
             this.$dropdown_items = $('.ui-people-picker-dropdown-item');
+            return this;
         },
 
         _bindEvent: function () {
             this.$input.on('focus', this._inputFocusHandler.bind(this))
-                .on('blur', this._inputBlurHandler.bind(this));
+                .on('blur', this._inputBlurHandler.bind(this))
+                .on('keyup', this._inputKeyupHandler.bind(this))
             this.$dropdown_items.on('click', this._downdropitemsClickHandler.bind(this));
         },
 
         _inputFocusHandler: function () {
+            this.$popup.show();
+            this._setPopupPosition();
+        },
+
+        _inputBlurHandler: function () {
+            this.$popup.hide();
+        },
+
+        _inputKeyupHandler: function (e) {
 
         },
-        _inputBlurHandler: function () {
-
-        }
+        _getCondition: function () {
+            return this.$input.val().toLowerCase();
+        },
         _setPopupPosition: function () {
             var self = this;
             this.$popup
@@ -979,6 +1006,7 @@
                     collision: "flip"
                 })
         },
+
         _downdropitemsClickHandler: function () {
 
         },
