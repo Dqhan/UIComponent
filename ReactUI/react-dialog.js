77ba01e7fce96e875609fb7627b6f679ddace6eb
Dqhan <291927006@qq.com>
2019-08-20 17:46:25 +0800

    commit

--- ReactUI/react-dialog.js
+++ ReactUI/react-dialog.js
@@ -1,39 +1,40 @@
-import ReactWidget from './react-widget';
+import ReactWidget from "./react-widget";
 class Dialog extends ReactWidget {
-    constructor(props) {
-        super(props);
-        this.state = {};
-        this.element = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.element = {};
+  }
 
-    componentWillReceiveProps(newProps) {
-        this.element.setOptions({
-            title: this.props.title,
-            width: this.props.width,
-            height: this.props.height,
-            status: newProps.status
-        });
-    }
+  componentWillReceiveProps(newProps) {
+    this.element.setOptions({
+      title: newProps.title,
+      width: newProps.width,
+      height: newProps.height,
+      status: newProps.status
+    });
+  }
 
-    componentDidMount() {
-        this.element = new aui.Dialog({
-            element: $(ReactDOM.findDOMNode(this)),
-            title: this.props.title,
-            width: this.props.width,
-            height: this.props.height,
-            footFragement: this.props.foot
-        });
-        var i = 0;
-        for (; i < this.props.foot.length; i++) {
-            $(ReactDOM.findDOMNode(this)).on('btnClick', this.props.foot[0].click)
-        };
+  componentDidMount() {
+    this.element = new aui.Dialog({
+      element: $(ReactDOM.findDOMNode(this)),
+      title: this.props.title,
+      width: this.props.width,
+      height: this.props.height,
+      footFragement: this.props.foot,
+      status: this.props.status
+    });
+    var i = 0;
+    for (; i < this.props.foot.length; i++) {
+      $(ReactDOM.findDOMNode(this)).on("btnClick", this.props.foot[0].click);
     }
+  }
 
-    render() {
-        return <div id={this.props.id || 'dialog-default-id'}>
-            {this.props.children}
-        </div>
-    }
+  render() {
+    return (
+      <div id={this.props.id || "dialog-default-id"}>{this.props.children}</div>
+    );
+  }
 }
 
 window.$$.Dialog = Dialog;
